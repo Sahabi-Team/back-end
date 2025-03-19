267d135758c3ae5a78febcde21edb7427662245a
GOOZIKHAN <mohammadhosseinpanbechi8908@gmail.com>
2025-03-19 22:31:48 +0330

    refactor: authentication view edit

--- authentication/views.py
+++ authentication/views.py
@@ -18,3 +18,12 @@ class UserDetailView(CreateAPIView):
         user = request.user
         serializer = self.serializer_class(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class SignupView(APIView):
+    def post(self, request):
+        serializer = ClientSignupSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
