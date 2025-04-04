99fd9ab5b93281cf7e53c4b8e00b3c3389c6d4c1
Imanghader <49917769+iman008@users.noreply.github.com>
2025-04-04 22:01:33 +0330

    added admin panel for workout

--- workout/admin.py
+++ workout/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import WorkoutPlan, WorkoutExercise
 
-# Register your models here.
+@admin.register(WorkoutPlan)
+class WorkoutPlanAdmin(admin.ModelAdmin):
+    list_display = ("trainer", "trainee", "created_at", "is_active")
+    list_filter = ("is_active", "created_at")
+    search_fields = ("trainer__user__username", "trainee__user__username")
+    ordering = ("-created_at",)
+
+@admin.register(WorkoutExercise)
+class WorkoutExerciseAdmin(admin.ModelAdmin):
+    list_display = ("workout_plan", "exercise", "sets", "reps", "duration")
+    list_filter = ("workout_plan", "exercise")
+    search_fields = ("workout_plan__trainer__user__username", "exercise__name")
