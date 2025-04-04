7843354392a8d719e428f8685fd7e3b4f5ad94ad
Imanghader <49917769+iman008@users.noreply.github.com>
2025-04-04 22:02:45 +0330

    Remove ignored files from repository

--- tests/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Test
-
-@admin.register(Test)
-class TestAdmin(admin.ModelAdmin):
-    list_display = ('trainee', 'goal', 'workout_room', 'workout_experience', 'created_at')
-    list_filter = ('goal', 'workout_experience', 'workout_room')
-    search_fields = ('trainee__user__username', 'goal')
-
--- tests/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import TestCreateView, TestListView
-
-urlpatterns = [
-    path('submit/', TestCreateView.as_view(), name='submit-test'),  # POST request
-    path('my-tests/', TestListView.as_view(), name='list-tests'),  # GET request
-]
