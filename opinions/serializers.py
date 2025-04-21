fa925b00416dd07391c2610fe55b37be28426336
Imanghader <49917769+iman008@users.noreply.github.com>
2025-04-21 21:56:39 +0330

    refactor: added rating and name to the apis and model

--- opinions/serializers.py
+++ opinions/serializers.py
@@ -2,7 +2,9 @@ from rest_framework import serializers
 from .models import Opinion
 
 class OpinionSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source='user.name', read_only=True)
+
     class Meta:
         model = Opinion
-        fields = ['id', 'user', 'text', 'created_at']
-        read_only_fields = ['user', 'created_at']
+        fields = ['id', 'user', 'text', 'rating', 'created_at', 'name']
+        read_only_fields = ['user', 'created_at', 'name']
