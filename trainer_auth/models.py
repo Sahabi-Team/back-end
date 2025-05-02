b73c7ad9e35d5221abc9099d0389a1e8b5e9f2b2
Imanghader <49917769+iman008@users.noreply.github.com>
2025-05-02 23:58:34 +0330

    trainer model bug fixed

--- trainer_auth/models.py
+++ trainer_auth/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from authentication.models import User
 
+
 class Trainer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='trainer_profile')
     bio = models.TextField(blank=True, default="")
@@ -11,12 +12,13 @@ class Trainer(models.Model):
     certificates = models.TextField(blank=True, default="")
 
     def __str__(self):
-        return str(self.rating())
+        return "bbbb"
 
     @property
     def rating(self):
         from math import ceil
         return (self.comments_received.aggregate(avg_rating=models.Avg('rating'))['avg_rating'] or 0.0)
+
 
 
 class Comment(models.Model):
