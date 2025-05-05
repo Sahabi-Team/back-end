d866d62d61df8744bd2ca3647f574bcdecbb089a
GOOZIKHAN <mohammadhosseinpanbechi8908@gmail.com>
2025-05-05 20:54:14 +0330

    chore: add cors stuff to fix front connection to back

--- sahabi/settings.py
+++ sahabi/settings.py
@@ -83,11 +83,13 @@ MIDDLEWARE = [
 ]
 
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:5173",
-#     "http://127.0.0.1:5173",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://84.234.29.28:5173",
+]
 
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'sahabi.urls'
 
