50c763e8cceccd0c43feb0a97f4640e42378228e
GOOZIKHAN <mohammadhosseinpanbechi8908@gmail.com>
2025-05-06 20:16:19 +0330

    chore: DEBUG = TRUE

--- sahabi/settings.py
+++ sahabi/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '605foi!lm@ctdu%@fzo9hsf(-+5=tm$kv-6wi_68szks=4ho54'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ighader.pythonanywhere.com','localhost','127.0.0.1', '84.234.29.28']
 
