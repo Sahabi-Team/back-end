4380a054f573bf70879b64c62a0b8fb99147508c
GOOZIKHAN <mohammadhosseinpanbechi8908@gmail.com>
2025-05-20 15:36:09 +0330

    feat: add pupil-tests API

--- tests/urls.py
+++ tests/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TestCreateView, TestListView
+from .views import TestCreateView, TestListView, TrainerPupilTestsView
 
 urlpatterns = [
     path('submit/', TestCreateView.as_view(), name='submit-test'),  # POST request
     path('my-tests/', TestListView.as_view(), name='list-tests'),  # GET request
+    path('pupil-tests/', TrainerPupilTestsView.as_view(), name='trainer-pupil-tests'),  # GET request
 ]
