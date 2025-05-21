b3c05a4ad6dbd6f94dd80209167c713ae57ef352
GOOZIKHAN <mohammadhosseinpanbechi8908@gmail.com>
2025-05-22 03:00:14 +0330

    fix: send mentorship_id in pupil test view

--- tests/serializers.py
+++ tests/serializers.py
@@ -1,15 +1,24 @@
 from rest_framework import serializers
 from .models import Test
+from mentorship.models import Mentorship
 
 class TestSerializer(serializers.ModelSerializer):
     trainee_username = serializers.CharField(source='trainee.user.username', read_only=True)  # Show username instead of ID
     trainee_name = serializers.CharField(source='trainee.user.name', read_only=True)
+    mentorship_id = serializers.SerializerMethodField()
+
+    def get_mentorship_id(self, obj):
+        mentorship = Mentorship.objects.filter(
+            trainee=obj.trainee,
+            is_active=True
+        ).first()
+        return mentorship.id if mentorship else None
 
     class Meta:
         model = Test
         # fields = '__all__'
         fields = [
-            'id', 'trainee', 'trainee_username', 'trainee_name', 'birth_date', 'gender', 'body_form',
+            'id', 'trainee', 'trainee_username', 'trainee_name', 'mentorship_id', 'birth_date', 'gender', 'body_form',
             'weight', 'height', 'goal_weight', 'goal', 'equipment', 'workout_days', 
             'diseases', 'focus_area', 'fitness_level', 'created_at'
         ]
