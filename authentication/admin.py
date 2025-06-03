6e651a7e3803a5f3ad1170632806f228db73faf5
Imanghader <49917769+iman008@users.noreply.github.com>
2025-06-03 22:07:24 +0330

    refactored the admin.py of users

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    model = User
 
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('name', 'email', 'phone_number', 'profile_picture')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('User Type', {'fields': ('usertype',)}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'phone_number', 'password1', 'password2', 'usertype'),
+        }),
+    )
+
+    list_display = ('username', 'email', 'name', 'phone_number', 'usertype', 'is_staff')
+    search_fields = ('username', 'email', 'name', 'phone_number')
+    ordering = ('username',)
