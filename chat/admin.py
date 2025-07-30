47fde78e105d78a4f36b2a1a11749f71090fb735
GooziKhan <jakoo8908@gmail.com>
2025-07-30 21:18:09 +0330

    feat: add admin panel for chat

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Message
 
-# Register your models here.
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'mentorship',
+        'sender',
+        'receiver',
+        'short_content',
+        'timestamp',
+        'seen',
+        'seen_at',
+    )
+    list_filter = ('seen', 'timestamp', 'mentorship')
+    search_fields = (
+        'sender__username',
+        'receiver__username',
+        'content',
+        'mentorship__id',
+    )
+    readonly_fields = ('timestamp',)
+    date_hierarchy = 'timestamp'
+
+    def short_content(self, obj):
+        return obj.content[:50]
+    short_content.short_description = 'Message'
